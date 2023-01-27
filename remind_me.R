2de26c145f146f6ee4d937f733ef8f26020b9f4e
selivs-ss <123744849+selivs-ss@users.noreply.github.com>
2023-01-27 14:36:09 +0100

    Add files via upload

---
+++ remind_me.R
@@ -0,0 +1,8 @@
+# remind_me(), takes no arguments
+# will remind you to buy bread
+
+remind_me <- function(){
+  groceries_reminder <- ("Don't forget to buy bread")
+  return(groceries_reminder)
+}
+remind_me()
